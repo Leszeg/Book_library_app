bcf90f1b78aba89f18d2e7809fb99996f2e45441
Leszeg <mazix1480@gmail.com>
2020-10-14 22:09:45 +0200

    Obsługa zapytań HTTP w połączeniu z bazą danych

--- book_library_app/authors.py
+++ book_library_app/authors.py
@@ -1,30 +1,41 @@
-from book_library_app import app
-from flask import jsonify
-from book_library_app.models import Author
+from book_library_app import app, db
+from flask import jsonify, request
+from book_library_app.models import Author, AuthorSchema, author_schema
+from webargs.flaskparser import use_args
+
 
 # dekorator route - informuje że funkcja ma zostać wywołana w momencie wejścia pod podanu adres url
 # Dekorator app.route rejestruje daną funkcjonalność dla danej regóły URL
 @app.route('/api/v1/authors', methods=['GET'])
 def get_authors():
+    authors = Author.query.all()
+    author_schema = AuthorSchema(many=True)
     return jsonify({
         'success': True,
-        'data': 'Get all authors'
+        'data': author_schema.dump(authors),
+        'number_of_records': len(authors)
     })
 
 
 @app.route('/api/v1/authors/<int:author_id>', methods=['GET'])
 def get_author(author_id: int):
+    author = Author.query.get_or_404(author_id, description=f"Author with id {author_id} not found")
+
     return jsonify({
         'success': True,
-        'data': f'Get single author with id {author_id}'
+        'data': author_schema.dump(author)
     })
 
 
 @app.route('/api/v1/authors', methods=['POST'])
-def create_author():
+@use_args(author_schema)
+def create_author(args: dict):
+    author = Author(**args)
+    db.session.add(author)
+    db.session.commit()
     return jsonify({
         'success': True,
-        'data': 'New author has been created'
+        'data': author_schema.dump(author)
     }), 201
 
 
--- book_library_app/models.py
+++ book_library_app/models.py
@@ -1,4 +1,6 @@
 from book_library_app import db
+from marshmallow import Schema, fields, validate, validates, ValidationError
+from datetime import datetime
 
 
 # Nie tworze konstruktora __init__ bo jest niejawnie tworzona przez -alchemy  **kwars
@@ -11,3 +13,18 @@ class Author(db.Model):
 
     def __repr__(self):
         return f'<{self.__class__.__name__}>: {self.first_name} {self.last_name}'
+
+
+class AuthorSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    first_name = fields.String(required=True, validate=validate.Length(max=50))
+    last_name = fields.String(required=True, validate=validate.Length(max=50))
+    birth_date = fields.Date('%d-%m-%Y', required=True)
+
+    @validates('birth_date')
+    def validate_birth_date(self, value):
+        if value > datetime.now().date():
+            raise ValidationError(f'Birth date must be lower than{datetime.now().date()}')
+
+
+author_schema = AuthorSchema()
